b3fe85b7a06168e5bdd9e8f248359f1885e0f0e8
VaheAA <vahe.abov@gmail.com>
2022-03-16 14:34:15 +0400

    api call

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>IP Tracker</h1>
 </template>
+<script setup>
+import { onMounted } from 'vue';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+onMounted(async () => {
+  const res = await fetch(
+    'https://geo.ipify.org/api/v2/country?apiKey=at_HE9nGhnpFkoIfoNLlwIchHCyRGajj'
+  );
+  const data = await res.json();
+  console.log(data);
+});
+</script>
