a91676707d6aac8b89858a4246d50c960d055765
VaheAA <vahe.abov@gmail.com>
2022-03-17 11:19:01 +0400

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Rubik', 'sans - serif']
+    },
+    colors: {
+      veryDarkGray: 'hsl(0, 0%, 17%)',
+      darkGray: 'hsl(0, 0%, 59%)'
+    },
     extend: {}
   },
   plugins: []
